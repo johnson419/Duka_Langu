55f4ae8e9da0688c88b7a13233f3676ed6001d9b
Johnson Fredrick <johnsonfredrick577@gmail.com>
2024-10-22 16:20:01 +0300

    removed unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,15 @@ import "./globals.css";
 import Topbar from "@/components/ui/Topbar";
 import Sidebar from "@/components/ui/Sidebar";
 
-export const meta: Metadata = { title: "Shop Management", description: "Shop Management" };
 
-export default function Home({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function Home() {
   return (
    <div className="bg-[#f6f7f9] flex h-screen overflow-x-hidden overflow-y-auto">
     <Sidebar />
     <main className="flex-1 flex flex-col">
     <div className="relative flex flex-col">
               <Topbar />
-              <div className="px-4 py-6 max-sm:px-4">{children}</div>
+              <div className="px-4 py-6 max-sm:px-4"></div>
               </div>
     </main>
    </div>
